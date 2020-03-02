9c84aec503f5d16402f87da4d06ceb09c4d4cb34
Ben Glick <glick@glick.cloud>
2020-03-02 13:00:26 -0800

    add stopping

--- data_processing/stopping_stemming/stopping_stemming.py
+++ data_processing/stopping_stemming/stopping_stemming.py
@@ -1,4 +1,4 @@
-import re
+import regex as re
 """
 Stopping and Stemming should help us
 reduce the data closer to purely semantic units.
@@ -12,4 +12,11 @@ def remove_long_tails(sequence):
 	One of the more frequent issues that shows up with data like this
 	is repeated subsequences - ie `eeeeeee` or `kkkkkkk` in sequence
 	"""
-	pass
+	tail_regex = r"([a-zA-Z0-9])\1{3,}"
+	all_matches = re.match(sequence)
+	for match in all_matches:
+		sequence.replace(match, "")
+
+	return sequence
+
+
