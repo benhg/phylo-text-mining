b712db3753f08f21a5103d6dcbb10f542b79f719
Ben Glick <glick@glick.cloud>
2020-04-11 20:02:44 -0700

    tabs -> spaces

--- data_processing/stopping_stemming/stopping_stemming.py
+++ data_processing/stopping_stemming/stopping_stemming.py
@@ -11,50 +11,50 @@ This module should help to filter out our equivalent of "stop-words"
 """
 
 def remove_long_tails(sequence):
-	"""
-	One of the more frequent issues that shows up with data like this
-	is repeated subsequences - ie `eeeeeee` or `kkkkkkk` in sequence
-	"""
-	tail_regex = r"(\w)\2+"
-	all_matches = re.findall(tail_regex, sequence)
-	for match in all_matches:
-		print(match)
-		sequence = sequence.replace(match, "")
+    """
+    One of the more frequent issues that shows up with data like this
+    is repeated subsequences - ie `eeeeeee` or `kkkkkkk` in sequence
+    """
+    tail_regex = r"(\w)\2+"
+    all_matches = re.findall(tail_regex, sequence)
+    for match in all_matches:
+        print(match)
+        sequence = sequence.replace(match, "")
 
-	return sequence
+    return sequence
 
 def remove_long_tails_no_regex(sequence):
-	"""
-	Do the same as above, but no regex
-	"""
-	letters = list(string.ascii_uppercase + string.ascii_lowercase)
-	for letter in letters:
-		for i in range(10, 4, -1):
-			letter_pattern = letter*i
-			if letter_pattern in sequence:
-				sequence = sequence.replace(letter_pattern, "")
-	return sequence
+    """
+    Do the same as above, but no regex
+    """
+    letters = list(string.ascii_uppercase + string.ascii_lowercase)
+    for letter in letters:
+        for i in range(10, 4, -1):
+            letter_pattern = letter*i
+            if letter_pattern in sequence:
+                sequence = sequence.replace(letter_pattern, "")
+    return sequence
 
 
 
 def fully_stem_and_lemmatize(sequence):
-	"""
-	Run all of the stemming/lemmatizing in a row
-	"""
-	sequence = remove_long_tails_no_regex(sequence)
-	return sequence
+    """
+    Run all of the stemming/lemmatizing in a row
+    """
+    sequence = remove_long_tails_no_regex(sequence)
+    return sequence
 
 def stem_and_lemmatize_in_fasta_file(fasta_file_location, output_file_location):
-	with open(output_file_location, "w") as fh:
-		for record in SeqIO.parse(handle, "fasta"):
+    with open(output_file_location, "w") as fh:
+        for record in SeqIO.parse(handle, "fasta"):
                 fh.write(f">{record.id}\n")
                 fh.write(str(fully_stem_and_lemmatize(str(record.seq))))
 
 
 
 if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		print(f"Usage: {sys.argv[0]} <Input file> <Output file>")
-		exit(1)
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} <Input file> <Output file>")
+        exit(1)
 
-	stem_and_lemmatize_in_fasta_file(sys.argv[1] sys.argv[2])
+    stem_and_lemmatize_in_fasta_file(sys.argv[1] sys.argv[2])
